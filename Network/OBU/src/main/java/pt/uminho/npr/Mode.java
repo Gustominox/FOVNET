0a96f4804236bdb8593c88268d48c481791d994e
Gustominox <agstcmps.111@gmail.com>
2025-05-13 16:25:44 +0100

    refactor: Direto to Direct

--- Network/OBU/src/main/java/pt/uminho/npr/Mode.java
+++ Network/OBU/src/main/java/pt/uminho/npr/Mode.java
@@ -1,12 +1,12 @@
 package pt.uminho.npr;
 
 public enum Mode {
-    DIRETO, // Direct mode
+    DIRECT, // Direct mode
     SEARCH; // Search mode
 
     public String get() {
         switch (this) {
-            case DIRETO:
+            case DIRECT:
                 return "Modo direto: Envia a mensagem diretamente ao veículo";
             case SEARCH:
                 return "Modo search: Inicia o processo de procura pelo veículo destino";
