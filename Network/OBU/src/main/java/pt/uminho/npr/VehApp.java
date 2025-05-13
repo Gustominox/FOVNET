3d0305780fed3976611ff36ba6d1dc06af6c7aa7
Gustominox <agstcmps.111@gmail.com>
2025-05-13 10:52:02 +0100

    fix: Only react to VehInfo if msg wast not mine

--- Network/OBU/src/main/java/pt/uminho/npr/VehApp.java
+++ Network/OBU/src/main/java/pt/uminho/npr/VehApp.java
@@ -149,59 +149,62 @@ public class VehApp extends AbstractApplication<VehicleOperatingSystem>
             VehInfoMessage fwdMsg = (VehInfoMessage) receivedMessage.getMessage();
             String senderId = fwdMsg.getSenderName();
 
-            // Atualizar vizinhança
-            NeighborInfo neighbor = new NeighborInfo(
-                    senderId,
-                    fwdMsg.getSenderPosition(),
-                    fwdMsg.getDistanceToRsu(),
-                    fwdMsg.getNumberOfHops(),
-                    fwdMsg.getHeading(),
-                    fwdMsg.getSpeed(),
-                    fwdMsg.getLaneId(),
-                    currentTime);
+            if (senderId != getOs().getId()) { // only react if message is not mine, this avoid loopbacks
 
-            getLog().infoSimTime(this, "Adding Veh: " + neighbor.toString() + " To Network Map");
+                // Atualizar vizinhança
+                NeighborInfo neighbor = new NeighborInfo(
+                        senderId,
+                        fwdMsg.getSenderPosition(),
+                        fwdMsg.getDistanceToRsu(),
+                        fwdMsg.getNumberOfHops(),
+                        fwdMsg.getHeading(),
+                        fwdMsg.getSpeed(),
+                        fwdMsg.getLaneId(),
+                        currentTime);
 
-            knownVehicleNeighbors.put(senderId, neighbor);
-            /**
-             * If msgId is Broadcast, the sender doesnt know about any RSU's
-             * I should Apply the forwarding logic here
-             * 1. try to find RSU
-             * 2. if not find veh with closest
-             * 3. if not BROADCAST
-             */
-            // Forward Info message if my id is equal to fwrdID of msg
-            if (fwdMsg.getForwarderId() == getOs().getId() || fwdMsg.getForwarderId() == "BROADCAST") {
-                getLog().infoSimTime(this, "Forwarding Message ");
+                getLog().infoSimTime(this, "Adding Veh: " + neighbor.toString() + " To Network Map");
 
-                String forwarderId = bestNeighbor();
-                if (forwarderId != "BROADCAST") {
+                knownVehicleNeighbors.put(senderId, neighbor);
+                /**
+                 * If msgId is Broadcast, the sender doesnt know about any RSU's
+                 * I should Apply the forwarding logic here
+                 * 1. try to find RSU
+                 * 2. if not find veh with closest
+                 * 3. if not BROADCAST
+                 */
+                // Forward Info message if my id is equal to fwrdID of msg
+                if (fwdMsg.getForwarderId() == getOs().getId() || fwdMsg.getForwarderId() == "BROADCAST") {
+                    getLog().infoSimTime(this, "Forwarding Message ");
 
-                    MessageRouting routing = getOs().getAdHocModule()
-                            .createMessageRouting()
-                            .channel(AdHocChannel.CCH) // Send on Control Channel
-                            .topological().broadcast()
-                            .build(); // Broadcast to nearby vehicles
+                    String forwarderId = bestNeighbor();
+                    if (forwarderId != "BROADCAST") {
 
-                    // Create the VehInfoMsg that is a copy of the message received, only changing
-                    // the forwarder Id
-                    VehInfoMessage message = new VehInfoMessage(
-                            routing,
-                            fwdMsg.getMessageId(),
-                            fwdMsg.getTime(),
-                            fwdMsg.getSenderName(),
-                            fwdMsg.getSenderPosition(),
-                            fwdMsg.getHeading(),
-                            fwdMsg.getSpeed(),
-                            fwdMsg.getLaneId(),
-                            fwdMsg.getDistanceToRsu(),
-                            fwdMsg.getNumberOfHops(),
-                            forwarderId);
+                        MessageRouting routing = getOs().getAdHocModule()
+                                .createMessageRouting()
+                                .channel(AdHocChannel.CCH) // Send on Control Channel
+                                .topological().broadcast()
+                                .build(); // Broadcast to nearby vehicles
 
-                    getOs().getAdHocModule().sendV2xMessage(message);
-                    getLog().infoSimTime(this, "Sent VehInfoMsg: " + message.toString());
-                } // else i dont know any rsu's so i dont BROADCAST
+                        // Create the VehInfoMsg that is a copy of the message received, only changing
+                        // the forwarder Id
+                        VehInfoMessage message = new VehInfoMessage(
+                                routing,
+                                fwdMsg.getMessageId(),
+                                fwdMsg.getTime(),
+                                fwdMsg.getSenderName(),
+                                fwdMsg.getSenderPosition(),
+                                fwdMsg.getHeading(),
+                                fwdMsg.getSpeed(),
+                                fwdMsg.getLaneId(),
+                                fwdMsg.getDistanceToRsu(),
+                                fwdMsg.getNumberOfHops(),
+                                forwarderId);
 
+                        getOs().getAdHocModule().sendV2xMessage(message);
+                        getLog().infoSimTime(this, "Sent VehInfoMsg: " + message.toString());
+                    } // else i dont know any rsu's so i dont BROADCAST
+
+                }
             }
         }
 
