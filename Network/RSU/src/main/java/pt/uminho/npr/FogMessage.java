d2ee7a51a66f98c743a58d2d0aca42fa333a4625
Gustominox <agstcmps.111@gmail.com>
2025-05-16 15:22:17 +0100

    tidy: Replicate changes in other modules

--- Network/RSU/src/main/java/pt/uminho/npr/FogMessage.java
+++ Network/RSU/src/main/java/pt/uminho/npr/FogMessage.java
@@ -4,23 +4,30 @@ import org.eclipse.mosaic.lib.objects.v2x.MessageRouting;
 import org.eclipse.mosaic.lib.objects.v2x.V2xMessage;
 
 public abstract class FogMessage extends V2xMessage implements Message {
-    // The mode field can now be defined as an instance variable
-    private Mode mode;
 
-    // Constructor to set the mode
-    public FogMessage(final MessageRouting routing, Mode mode) {
+    private Mode mode;
+    private String forwarderId;
+
+    public FogMessage(final MessageRouting routing, Mode mode, String fid) {
         super(routing);
         this.mode = mode;
+        this.forwarderId = fid;
     }
 
-    // Getter method for mode
     public Mode getMode() {
         return mode;
     }
 
-    // Setter method for mode
     public void setMode(Mode mode) {
         this.mode = mode;
+    }
+
+    public String getFwrdId() {
+        return forwarderId;
+    }
+
+    public void setFwrdId(String fid) {
+        this.forwarderId = fid;
     }
 
 }
